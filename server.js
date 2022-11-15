f4f5ba1c29ebcfb3f0e559cf83f626f06c44a834
Shalaby <abdoshalaby.dev@gmail.com>
2022-11-15 15:37:35 +0200

    update index

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 
 const express = require('express')
 const path = require("path")
-const publicDir = path.join(__dirname, "./public");
+const publicDir = path.join(__dirname, "/public");
+console.log(publicDir)
 const app = express()
 const port = 8000
 
+
+
+
+
 app.use(express.static(publicDir));
 app.set("view engine", "hbs");
-
-
-
 app.use("/", require("./routes/pages"));
 
 app.listen(port, () => {
