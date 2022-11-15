b28fcaf0ce0ef2ca3627c462d7057bdad3796c12
Shalaby <abdoshalaby.dev@gmail.com>
2022-11-15 18:24:44 +0200

    add new files

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-
 const express = require('express')
 const path = require("path")
 const app = express()
 const publicDir = path.join(__dirname, "./public");
-
 const port = 8000
-
-
-
-
-
 app.use(express.static(publicDir));
 app.set("view engine", "hbs");
 app.use("/", require("./routes/pages"));
